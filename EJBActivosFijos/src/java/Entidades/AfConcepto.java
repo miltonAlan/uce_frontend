cf8d5fdcd059638d4f1c209550b6c85f59db4d34
codingWhileAlive <codingwhilealive@gmail.com>
2022-03-13 00:12:17 -0500

    MPaucar feat: Se agregan pantalla para modificaciones de conceptos

--- EJBActivosFijos/src/java/Entidades/AfConcepto.java
+++ EJBActivosFijos/src/java/Entidades/AfConcepto.java
@@ -8,18 +8,17 @@ import java.io.Serializable;
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
 
 /**
  *
- * @author mpaucar
+ * @author User
  */
 @Entity
 @Table(name = "af_concepto")
@@ -33,8 +32,8 @@ import javax.xml.bind.annotation.XmlRootElement;
 public class AfConcepto implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Basic(optional = false)
+    @NotNull
     @Column(name = "ac_consecutivo")
     private Integer acConsecutivo;
     @Size(max = 255)
